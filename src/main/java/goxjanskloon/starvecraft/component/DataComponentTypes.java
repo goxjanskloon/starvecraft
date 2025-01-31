c604a7cb1e0dcf0f5e1834aa5e1ccb270e1f77f9
goxjanskloon <goxjanskloon@outlook.com>
2025-01-31 22:33:10 +0800

    In progress

---
+++ src/main/java/goxjanskloon/starvecraft/component/DataComponentTypes.java
@@ -0,0 +1,14 @@
+package goxjanskloon.starvecraft.component;
+import net.minecraft.component.ComponentMap;
+import net.minecraft.component.ComponentType;
+import net.minecraft.component.type.AttributeModifiersComponent;
+import net.minecraft.component.type.ItemEnchantmentsComponent;
+import net.minecraft.component.type.LoreComponent;
+import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.dynamic.Codecs;
+import static net.minecraft.component.DataComponentTypes.*;
+public interface DataComponentTypes{
+    ComponentType<Float> WETNESS=net.minecraft.component.DataComponentTypes.register("wetness",builder->builder.codec(Codecs.NON_NEGATIVE_FLOAT).packetCodec(PacketCodecs.FLOAT));
+    ComponentMap DEFAULT_ITEM_COMPONENTS=ComponentMap.builder().add(MAX_STACK_SIZE,64).add(LORE,LoreComponent.DEFAULT).add(ENCHANTMENTS, ItemEnchantmentsComponent.DEFAULT).add(REPAIR_COST,0).add(ATTRIBUTE_MODIFIERS,AttributeModifiersComponent.DEFAULT).add(RARITY,Rarity.COMMON).add(DataComponentTypes.WETNESS,0f).build();
+}
---
+++ src/main/java/goxjanskloon/starvecraft/mixin/DataComponentTypesMixin.java
@@ -0,0 +1,12 @@
+package goxjanskloon.starvecraft.mixin;
+import goxjanskloon.starvecraft.component.DataComponentTypes;
+import net.minecraft.component.ComponentMap;
+import net.minecraft.component.type.AttributeModifiersComponent;
+import net.minecraft.component.type.ItemEnchantmentsComponent;
+import net.minecraft.component.type.LoreComponent;
+import net.minecraft.util.Rarity;
+import org.spongepowered.asm.mixin.Mixin;
+import static net.minecraft.component.DataComponentTypes.*;
+@Mixin(DataComponentTypes.class) public class DataComponentTypesMixin{
+    @
+}
